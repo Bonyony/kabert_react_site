6e2c66356cd825cc195314428d97bb57ef33c68c
Frank Francione <frankiefrancione@gmail.com>
2024-06-18 11:13:58 -0500

    asd

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,22 +7,34 @@ const Menu = () => {
     <>
       <ul className="flex items-center gap-4">
         <li>
-          <a className="hover:text-gray-900" href="#home">
+          <a
+            className="hover:text-gray-900 border-black hover:border-b-4"
+            href="#home"
+          >
             Home
           </a>
         </li>
         <li>
-          <a className="hover:text-gray-900" href="#about">
+          <a
+            className="hover:text-gray-900 border-black hover:border-b-4"
+            href="#about"
+          >
             About Us
           </a>
         </li>
         <li>
-          <a className="hover:text-gray-900" href="#products">
+          <a
+            className="hover:text-gray-900 border-black hover:border-b-4"
+            href="#products"
+          >
             Our Products
           </a>
         </li>
         <li>
-          <a className="hover:text-gray-900" href="#contact">
+          <a
+            className="hover:text-gray-900 border-black hover:border-b-4"
+            href="#contact"
+          >
             Contact Us
           </a>
         </li>
@@ -33,7 +45,7 @@ const Menu = () => {
 
 const Header = () => {
   return (
-    <div className="kabert__header font-bold">
+    <div className="kabert__header font-bold rounded-b-3xl">
       <div className="kabert__links-container flex justify-between items-center p-5 mx-auto">
         <div className="kabert__logo text-2xl">
           {/* Place a logo here that animates to the Kabert Industries name */}
